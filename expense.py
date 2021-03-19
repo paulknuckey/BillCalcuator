2833d5a49329893b734dc145c9425e05475fa579
Paul Knuckey <68744477+paulknuckey@users.noreply.github.com>
2021-03-19 10:12:19 -0500

    Update expense.py
    if input is left empty value of 0 will be returned

--- expense.py
+++ expense.py
@@ -8,67 +8,67 @@
 
 
 print("Monthly Bill Calualtor")
-print("If no payment is required enter 0")
+#print("If no payment is required enter 0") 
 print("")
 #This is done through most of the app several times
 #it's all the same
-#collect ammont, convert string to float
-pay = float(input("How much did you get paid? $"))
+#collect ammont, convert string to float if no value is entered zero will be returned
+pay = float(input("How much did you get paid? $") or "0")
 #test the conversion took
 #print(type(pay))
 
-day_care = float(input("Day Care Payment: $"))
+day_care = float(input("Day Care Payment: $") or "0")
 #input(type(day_care))
 
-house = float(input("House Payment: $"))
+house = float(input("House Payment: $") or "0")
 #print(type(house))
 
-web = float(input("Internet Payment: $"))
+web = float(input("Internet Payment: $") or "0")
 #print(type(web))
 
-hulu = float(input("Hulu Payment: $"))
+hulu = float(input("Hulu Payment: $") or "0")
 #print(type(hulu))
 
-xm = float(input("Xm Radio payment: $"))
+xm = float(input("Xm Radio payment: $") or "0")
 #print(type(xm))
 
-car = float(input("Car Payment: $"))
+car = float(input("Car Payment: $") or "0")
 #print(type(car))
 
-netflix = float(input("Netflix Payment: $"))
+netflix = float(input("Netflix Payment: $") or "0")
 #print(type(netflix))
 
-cameo = float(input("Cameo Payment: $"))
+cameo = float(input("Cameo Payment: $") or "0")
 #print(type(cameo))
 
-ny_life = float(input("NY Life Insurance payment: $"))
+ny_life = float(input("NY Life Insurance payment: $") or "0")
 #print (type(ny_life))
 
-health = float(input("Health Payment: $"))
+health = float(input("Health Payment: $") or "0")
 #print(type(health))
 
-car_insure = float(input("Car Insurance payment: $"))
+car_insure = float(input("Car Insurance payment: $") or "0")
 #print(type(car_insure))
 
-massage = float(input("Massage Envy Payment: $"))
+massage = float(input("Massage Envy Payment: $") or "0")
 #print(type(massage))
 
-water = float(input("Water Payment: $"))
+water = float(input("Water Payment: $") or "0")
 #print(type(water))
 
-gas = float(input("Gas payment: $"))
+gas = float(input("Gas payment: $") or "0")
 #print(type(gas))
 
-fuel = float(input("Fuel Cost: $"))
+fuel = float(input("Fuel Cost: $") or "0")
 #print(type(fuel))
 
-phone = float(input("Phone Bill: $"))
+phone = float(input("Phone Bill: $") or "0")
 #print(type(phone))
 
-grocery = float(input("Grocery cost: $"))
+grocery = float(input("Grocery cost: $") or "0")
 #print(type(grocery))
 
-unplan = float(input("Unplaned Expenses: $"))
+unplan = float(input("Unplaned Expenses: $") or "0")
 #print(type(unplan))
 
 #Calculations performed and inserted in expence varitable
@@ -89,3 +89,4 @@ left_pay = float(pay - expense)
 print("")
 left_pay = "{:.2f}".format(left_pay)
 print(f"Money after expenses ${left_pay}")
+print("")
